17a119a4c4d48efcb99116b6101ae727f301b471
John Edward Torres <set>
2024-09-12 23:11:01 +0800

    feat: build

--- service/LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
+++ service/LibraryAPI/obj/Debug/net8.0/LibraryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b30ffc1638090170451b2210cc2fdfc6efc09b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d71fb2be218e318563634285c3196c9c0bf8bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
