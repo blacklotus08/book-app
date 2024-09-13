9dd6104502146ed66c43a8d58ae1adc1ef26a542
John Edward Torres <set>
2024-09-13 22:14:13 +0800

    feat: setup gitignore

--- service/Library.Api/obj/Debug/net8.0/Library.Api.AssemblyInfo.cs
+++ service/Library.Api/obj/Debug/net8.0/Library.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ee80ab0847115b58e6527e27b8ed81e072a7f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b66677918681046ddc509b70d034dcbb1dc0a4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
