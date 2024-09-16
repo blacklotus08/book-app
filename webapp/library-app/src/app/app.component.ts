89a78af93193005d17382388914cbf0215b3eb72
John Edward Torres <set>
2024-09-16 21:52:54 +0800

    fix: Fix adding BrowseAnimation

--- webapp/library-app/src/app/app.component.ts
+++ webapp/library-app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
 
 @Component({
   standalone: true,
@@ -7,6 +9,7 @@ import { RouterModule } from '@angular/router';
   selector: 'la-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [MessageService, DialogService]
 })
 export class AppComponent {
   title = 'Library App';
--- webapp/library-app/src/app/app.config.ts
+++ webapp/library-app/src/app/app.config.ts
@@ -2,19 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-import { MessageService } from 'primeng/api';
-import { DialogService } from 'primeng/dynamicdialog';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideHttpClient(),
-    MessageService,
-    DialogService,
-    BrowserModule,
-    BrowserAnimationsModule
+    provideAnimations()
   ],
 };
